8755a078519e72252a756f07d75206d861bc9959
ajstudentdev <a.wijnia@st.hanze.nl>
2023-01-14 16:10:35 +0100

    change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=800, initial-scale=1.0">
+        <meta name="viewport" content="width=800; user-scalable=0;" />
+
         <script src="{{asset('incl/jquery.js')}}"></script>
         <title>Colloquium</title>
     </head>
